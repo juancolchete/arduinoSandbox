85c07788438221dd68a4b89b6aea9bb56d9dce3a
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-01-01 20:40:42 -0300

    fix: minutes and seconds

--- newYearEve/newYearEve.ino
+++ newYearEve/newYearEve.ino
@@ -24,7 +24,7 @@ void loop() {
   }else{
     m5.Lcd.fillScreen(BLACK);
     M5.Lcd.setCursor(0, 10);
-    M5.Lcd.printf("New Year in \n %02d hours\n %02d seconds \n %02d minutes",23-hour,59-minute,59-second);
+    M5.Lcd.printf("New Year in \n %02d hours\n %02d minutes \n %02d seconds",23-hour,59-minute,59-second);
   }
   delay(1000);
 }
