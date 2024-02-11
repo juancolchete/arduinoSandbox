5a5cd62b4fa8a31fca37be2f84774e2ca361054d
Juan Colchete <38388534+juancolchete@users.noreply.github.com>
2024-02-11 15:39:58 -0300

    chore: wait 1 second before change status

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -5,13 +5,13 @@
 
 
 void setup() {
-  M5.begin(true, true, true);
+  M5.begin();
   M5.Lcd.setTextSize(3);
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
       m5.Lcd.fillScreen(WHITE);
-      delay(500);
+      delay(1000);
       m5.Lcd.fillScreen(BLACK);
   }
   M5.Lcd.print("Connected");
